1c880984e8a392f1db6032e0813ff09409d0f6cd
Dayvid Jonathan <dayvidaf7@gmail.com>
2024-07-16 20:38:13 -0300

    Implementando controle de acesso

--- src/main/java/com/aluraflix/security/SecurityConfigurations.java
+++ src/main/java/com/aluraflix/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.aluraflix.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,25 +11,35 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+	
+	@Autowired
+	private SecurityFilter securityFilter;
  
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-	    return http.csrf(csrf -> csrf.disable())
-	            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-	            .build();
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    return 
+	        http.csrf(csrf -> csrf.disable())
+	        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	        .authorizeHttpRequests(req -> {
+	        	req.requestMatchers("/login").permitAll();
+	            req.anyRequest().authenticated();
+	        })
+	        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+	        .build(); 
 	}
 	
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
 		return configuration.getAuthenticationManager();
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
--- src/main/java/com/aluraflix/service/TokenService.java
+++ src/main/java/com/aluraflix/service/TokenService.java
@@ -11,6 +11,7 @@ import com.aluraflix.model.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 @Service
 public class TokenService {
@@ -33,6 +34,19 @@ public class TokenService {
 		
 	}
 
+	public String getSubject(String tokenJWT) {
+		try {
+			var algoritmo = Algorithm.HMAC256(secret);
+		    return JWT.require(algoritmo)
+		    	.withIssuer("Alura-FLix API")
+		        .build()
+		        .verify(tokenJWT)
+		        .getSubject();	    
+		} catch (JWTVerificationException exception){
+		    throw new RuntimeException("Token JWT inválido ou expirado");
+		}
+	}
+	
 	private Instant dataExpiracao() {
 		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
 	}
